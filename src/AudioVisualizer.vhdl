eb9f48dd8b44f21ce8daaf9f48611319bfa5cd4f
Johannes Kauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-05-12 13:08:04 +0200

    Tabs are now 4 instead of 3 spaces.

--- src/AudioVisualizer.vhdl
+++ src/AudioVisualizer.vhdl
@@ -5,12 +5,12 @@ USE ieee.std_logic_1164.all;
 
 
 ENTITY AudioVisualizer IS
-   PORT( x1, x2, x3  :  IN  STD_LOGIC;
-         f           :  OUT STD_LOGIC);
+    PORT(   x1, x2, x3  :  IN  STD_LOGIC;
+            f           :  OUT STD_LOGIC);
 END AudioVisualizer;
 
 
 ARCHITECTURE AudioVisualizer_rtl OF AudioVisualizer IS
 BEGIN
-   f <= (x1 AND x2) OR (x1 AND x3) OR (x2 AND x3);
+    f <= (x1 AND x2) OR (x1 AND x3) OR (x2 AND x3);
 END AudioVisualizer_rtl;
