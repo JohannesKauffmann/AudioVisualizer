c8bf6467e76346f8f88e109120338bd2e39f3510
Johannes Kauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-05-12 12:49:44 +0200

    Added majority3 vhdl example. Updated gitignore.

---
+++ src/AudioVisualizer.vhdl
@@ -0,0 +1,16 @@
+-- Contains majority3 example - for now.
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+
+ENTITY AudioVisualizer IS
+	PORT( x1, x2, x3	:	IN  STD_LOGIC;
+			f:					OUT STD_LOGIC);
+END AudioVisualizer;
+
+
+ARCHITECTURE AudioVisualizer_rtl OF AudioVisualizer IS	
+BEGIN
+	f <= (x1 AND x2) OR (x1 AND x3) OR (x2 AND x3);
+END AudioVisualizer_rtl;
