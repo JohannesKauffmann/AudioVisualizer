1aae3c162bcb9aad2ed1af9edffd88e58ddbc74f
Johannes Kauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-05-13 09:55:31 +0200

    Removed BSP project and binaries, added BSP script and updated README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AudioVisualizer
 Repository to be used for the FPGA course at NHL Stenden.
+
+## Getting started
+First, the software BSP project has to generated and compiled. To do this, open the NIOS II Command Shell from the start menu and run the `generate_bsp.sh` script.
---
+++ generate_bsp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "welcome!"
+
+if [ -z "${SOPC_KIT_NIOS2}" ]; then
+    echo "Please run the NIOS II Command Shell first!" && sleep 5 && exit 1
+fi
+
+QUARTUS_PROJECT_DIR=`realpath $(dirname "$0")`
+SOFTWARE_BASE_DIR="${QUARTUS_PROJECT_DIR}/software"
+
+SOFTWARE_PROJECT_NAME="SampleProject"
+
+#todo: paths en shit mooi maken, updaten en rebuild van project??
+
+#test if bsp has been generated
+if [ ! -d "${SOFTWARE_BASE_DIR}/${SOFTWARE_PROJECT_NAME}_bsp" ]; then
+    # Somehow nios2-bsp doesnt work unless the bsp-dir is "."
+    mkdir "${SOFTWARE_BASE_DIR}/${SOFTWARE_PROJECT_NAME}_bsp" && cd "${SOFTWARE_BASE_DIR}/${SOFTWARE_PROJECT_NAME}_bsp" && nios2-bsp hal . "${QUARTUS_PROJECT_DIR}/nios2_subsystem.sopcinfo" --cpu-name nios2_gen2 && make && cd $OLDPWD
+fi
