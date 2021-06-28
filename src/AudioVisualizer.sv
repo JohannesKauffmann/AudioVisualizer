b5077aff3f6aba4788d91135f9fb434b96f22691
Rik2001 <rikvanrijn2001@gmail.com>
2021-06-28 15:00:11 +0200

    Refactored top-level and removed pin assignments

--- src/AudioVisualizer.sv
+++ src/AudioVisualizer.sv
@@ -1,9 +1,6 @@
 module AudioVisualizer
 (
-    input   logic   x1,
-                    x2,
-                    x3,
-                    CLOCK_50,
+    input   logic   CLOCK_50,
 
                     AUD_ADC_LRCK,   // Audio left/right clock
                     AUD_BCLK,       // Audio bitclock
@@ -11,110 +8,67 @@ module AudioVisualizer
 
     inout           I2C_SDAT,
 
-    output  logic   f,
-                    ADV_BLANK_N,
-                    ADV_SYNC_N,
-                    VGA_HS,
-                    VGA_VS,
-                    VGA_CLK,
+    output  logic   ADV_BLANK_N,		//VGA blanking
+                    ADV_SYNC_N,		//VGA sync
+                    VGA_HS,			//VGA horizontal sync
+                    VGA_VS,			//VGA vertical sync
+                    VGA_CLK,			//VGA clock 25,175 mhZ
 
                     I2C_SCLK,
                     AUD_XCK,        // Audio master clock
-
+	 //VGA data
     output logic [7:0] VGA_R,
     output logic [7:0] VGA_G,
-    output logic [7:0] VGA_B,
-    
-    output logic clk_out,
-    output logic clk_out2,
-    output logic gnd,
-    
-    output logic [5:0] greenLeds
+    output logic [7:0] VGA_B
 );
-
-    assign clk_out = control_bit;
-    assign clk_out2 = data_back;
-    assign gnd = 1'b0;
-
-    wire [31:0] data_sig, q_sig;    // Wires used to connect the FIFO input and output data
+	 // Wires used to connect the FIFO input and output data
+    wire [31:0] data_sig, q_sig;    
+	 
+	 //Used for FIFO module
     wire data_back, wrreq_sig, rdreq_sig;
-
-    // PLL for the VGA clock and Audio master clock
-    pll pll_inst (
-        .inclk0 (CLOCK_50),
-        .c0     (VGA_CLK),
-        .c1     (AUD_XCK)
-    );
-    
-    wire [5:0] height_in[19:0];
-
-    // instantiate vga controller
-    VGA_Controller vga_inst (
-        .CLK        (CLOCK_50),
-        .VGA_CLK    (VGA_CLK),
-        .control_bit(control_bit),
-        .height     (height_in),
-        .ADV_BLANK_N(ADV_BLANK_N),
-        .ADV_SYNC_N (ADV_SYNC_N),
-        .VGA_HS     (VGA_HS),
-        .VGA_VS     (VGA_VS),
-        .VGA_R      (VGA_R),
-        .VGA_G      (VGA_G),
-        .VGA_B      (VGA_B)
-    );
-
-    fifo fifo_inst (
-        .data       ( data_sig ),
-        .wrclk      ( AUD_BCLK ),
-        .wrreq      ( wrreq_sig ),
-        .wrempty    ( wrempty_sig ),
-        .wrfull     ( wrfull_sig ),
-
-        .q          ( q_sig ),
-        .rdclk      ( CLOCK_50 ),
-        .rdreq      ( rdreq_sig ),
-        .rdempty    ( rdempty_sig ),
-        .rdfull     ( rdfull_sig )
-    );
-    
+	 //Used for RAM module
     wire [5:0] ram_data_sig, ram_q_sig, ram_rdaddress_sig, ram_wraddress_sig;
-
-    vga_ram	vga_ram_inst (
-        .clock      ( CLOCK_50 ),
-        .data       ( ram_data_sig ),
-        .rdaddress  ( ram_rdaddress_sig ),
-        .wraddress  ( ram_wraddress_sig ),
-        .wren       ( ram_wren_sig ),
-        .q          ( ram_q_sig )
-	);
-
-    logic control_bit = 1'b0;
-    logic [5:0] height[19:0];
+	 
+	 //control bit is used in VGA_Controller for deciding which array to read and display on screen
+	 logic control_bit = 1'b0;
+	 
+	 //array's with height of every chart-bar
+	 logic [5:0] height[19:0];
+	 logic [5:0] height_in[19:0];
+	 
+	 //counter used for RAM case
     logic [4:0] counter = 5'd0;
 
-    logic canRead = 1'b0;
-    logic finished = 1'b0;
+	 //when can_read is 1 ram has been writen and can be read without the data changing while reading.
+    logic can_read = 1'b0;
     
+	 //is used in combination with data_back to detect the posedge of data_back;
     logic data_back_old = 1'b0;
     
+	 //counter for RAM adress
     logic [6:0] address_counter = 6'd0;
+	 
+	 //reset for audio_config and nios
+    wire reset_n;
+    assign reset_n = 1'b1;
 
     always_ff @ (posedge CLOCK_50)
     begin
-        control_bit <= ~canRead;
+        control_bit <= ~can_read;
         data_back_old <= data_back;
         
+		  //check for posedge of data_back
         if (data_back_old == 1'b0 && data_back == 1'b1)
-            canRead <= 1'b1;
+            can_read <= 1'b1;
         
-        if (canRead == 1'b1)
+        if (can_read == 1'b1)
         begin
             counter             <= counter + 5'd1;
             address_counter     <= address_counter + 6'd1;
             ram_rdaddress_sig   <= address_counter;
 
             case (counter)
-					// begin case at three because it takes 3 clocksignals before ram is red;
+					// begin case at three because it takes 3 clocksignals before ram is read;
                 5'd3:   begin
                             height[counter - 3]   = ram_q_sig;
                         end
@@ -138,7 +92,7 @@ module AudioVisualizer
                 5'd21:  height[counter - 3]   = ram_q_sig;
                 5'd22:  height[counter - 3]   = ram_q_sig;
                 5'd23:  begin
-                            canRead <= 1'b0;
+                            can_read <= 1'b0;
                             counter <= 5'd0;
                             address_counter <= 1'd0;
                             height_in <= height;
@@ -147,9 +101,53 @@ module AudioVisualizer
             
         end
     end
+	 
+    // PLL for the VGA clock and Audio master clock
+    pll pll_inst (
+        .inclk0 (CLOCK_50),
+        .c0     (VGA_CLK),
+        .c1     (AUD_XCK)
+    );
 
-    wire nios2_reset_n;
-    assign nios2_reset_n = 1'b1;
+    // instantiate vga controller
+    VGA_Controller vga_inst (
+        .CLK        (CLOCK_50),
+        .VGA_CLK    (VGA_CLK),
+        .control_bit(control_bit),
+        .height     (height_in),
+        .ADV_BLANK_N(ADV_BLANK_N),
+        .ADV_SYNC_N (ADV_SYNC_N),
+        .VGA_HS     (VGA_HS),
+        .VGA_VS     (VGA_VS),
+        .VGA_R      (VGA_R),
+        .VGA_G      (VGA_G),
+        .VGA_B      (VGA_B)
+    );
+
+	 //instantiate FIFO
+    fifo fifo_inst (
+        .data       ( data_sig ),
+        .wrclk      ( AUD_BCLK ),
+        .wrreq      ( wrreq_sig ),
+        .wrempty    ( wrempty_sig ),
+        .wrfull     ( wrfull_sig ),
+
+        .q          ( q_sig ),
+        .rdclk      ( CLOCK_50 ),
+        .rdreq      ( rdreq_sig ),
+        .rdempty    ( rdempty_sig ),
+        .rdfull     ( rdfull_sig )
+    );
+
+	 //instantiate VGA RAM
+    vga_ram	vga_ram_inst (
+        .clock      ( CLOCK_50 ),
+        .data       ( ram_data_sig ),
+        .rdaddress  ( ram_rdaddress_sig ),
+        .wraddress  ( ram_wraddress_sig ),
+        .wren       ( ram_wren_sig ),
+        .q          ( ram_q_sig )
+	);
 
     // instantiate Nios II subsystem
     nios2_subsystem nios2_inst (
@@ -162,16 +160,18 @@ module AudioVisualizer
         .pio_ram_data_external_connection_export       ( ram_data_sig ),
         .pio_ram_wraddress_external_connection_export  ( ram_wraddress_sig ),
         .pio_ram_wren_external_connection_export       ( ram_wren_sig ),
-        .reset_reset_n                                 ( nios2_reset_n )
+        .reset_reset_n                                 ( reset_n )
     );
 
+	 //instantiate audio config
     audio_config audio_config_inst (
         .clk        ( CLOCK_50 ),
-        .reset      ( ~nios2_reset_n ),
+        .reset      ( ~reset_n ),
         .i2c_data   ( I2C_SDAT ),
         .i2c_clk    ( I2C_SCLK )
     );
 
+	 //instantiate Audio controller
     AudioController aud_ctrl_inst (
         .AUD_ADC_CLK    (AUD_ADC_LRCK),
         .AUD_BCLK       (AUD_BCLK),
